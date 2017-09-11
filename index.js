880b18fe9af4a8a74a525b29272c30ad08ae6566
austinkelleher <austin.kell47@gmail.com>
2017-09-11 17:42:49 -0400

    Fixes #1 - Models that have properties that are type Model should be walked fully and the child properties should be defaulted as well.

--- index.js
+++ index.js
@@ -3,21 +3,41 @@ module.exports = {
 
   initType: function(Type) {
     Type.prototype.applyDefaults = function() {
-      var self = this;
+      function forEachProperty (_type, ctx) {
+        var didSetProps = false;
 
-      Type.forEachProperty(function(property) {
-        var name = property.getName();
-        var value = self.get(name);
+        _type.forEachProperty(function(property) {
+          var name = property.getName();
+          var value = ctx.get(name);
 
-        if (value === undefined) {
-          var defaultValue = property.default;
+          if (value === undefined) {
+            var defaultValue = property.default;
+            var defaultValueUndefined = typeof defaultValue === 'undefined';
+            var type = property.getType();
 
-          if ((defaultValue != null) && (defaultValue.constructor === Function)) {
-            defaultValue = defaultValue.call(self);
+            if (defaultValueUndefined && type.Model) {
+              // Temporarily set this to an empty object, so we can walk the sub
+              // properties
+              ctx.set(name, {});
+              var setProperties = forEachProperty(type, ctx.get(name));
+
+              if (!setProperties) ctx.set(name, undefined);
+              return;
+            }
+
+            if (!defaultValueUndefined && defaultValue !== null && defaultValue.constructor === Function) {
+              defaultValue = defaultValue.call(ctx);
+            }
+
+            ctx.set(name, defaultValue);
+            didSetProps = true;
           }
-          self.set(name, defaultValue);
-        }
-      });
+        });
+
+        return didSetProps;
+      }
+
+      forEachProperty(Type, this);
     };
   }
 };
--- test/test.js
+++ test/test.js
@@ -81,4 +81,129 @@ describe('apply defaults tests', function() {
       age: 30
     });
   });
+
+  it('should allow setting default to undefined', function () {
+    var Person = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        name: {
+          type: String,
+          default: undefined
+        }
+      }
+    });
+
+    var bob = new Person();
+
+    bob.applyDefaults();
+
+    expect(bob.clean()).to.deep.equal({});
+  });
+
+  it('should allow setting default value to null', function () {
+    var Person = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        name: {
+          type: String,
+          default: null
+        }
+      }
+    });
+
+    var bob = new Person();
+
+    bob.applyDefaults();
+
+    expect(bob.clean()).to.deep.equal({
+      name: null
+    });
+  });
+
+  it('should apply defaults to models that have sub model properties', function () {
+    var Person = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        name: {
+          type: String,
+          default: 'Bob'
+        }
+      }
+    });
+
+    var Car = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        owner: Person
+      }
+    });
+
+    var car = new Car();
+
+    car.applyDefaults();
+
+    expect(car.clean()).to.deep.equal({
+      owner: {
+        name: 'Bob'
+      }
+    });
+  });
+
+  it('should not call a sub model that has its own default and no value', function () {
+    var Person = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        name: {
+          type: String,
+          default: 'Bob'
+        }
+      }
+    });
+
+    var Car = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        owner: {
+          type: Person,
+          default: {
+            name: 'Bob'
+          }
+        }
+      }
+    });
+
+    var car = new Car();
+
+    car.applyDefaults();
+
+    expect(car.clean()).to.deep.equal({
+      owner: {
+        name: 'Bob'
+      }
+    });
+  });
+
+  it('should reset value of set object from empty object to undefined if no defaults found in sub model', function () {
+    var Person = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        name: String
+      }
+    });
+
+    var Car = Model.extend({
+      mixins: [DefaultsMixin],
+      properties: {
+        owner: {
+          type: Person
+        }
+      }
+    });
+
+    var car = new Car();
+
+    car.applyDefaults();
+
+    expect(car.clean()).to.deep.equal({});
+  });
 });
